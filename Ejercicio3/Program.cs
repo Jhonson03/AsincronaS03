2786c683271404e93467d1f6ea4ccdd11d8a8794
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-08-16 22:08:41 -0600

    Ejercicio3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Ejercicio3
+
+using System.Threading.Tasks;
+String[] Palabras = { "Perro", "Casa", "Cebra", "Tortuga", 
+    "Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol" };
+
+Console.WriteLine("Arreglo sin ordenar: ");
+for(int i = 0; i < Palabras.Length; i++)
+{
+    Console.WriteLine($"Posicion {i+1}: {Palabras[i]}");
+}
+
+Array.Sort(Palabras);
+
+Console.WriteLine("\nArreglo ordenado: ");
+
+foreach(string i in Palabras)
+{
+    Console.WriteLine(i);
+}
